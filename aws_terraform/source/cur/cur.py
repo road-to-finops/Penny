d90c70c2248d63324e6ddf8cb1cd3c9bf65ff879
Gooch <awssteph@f0189803d501.ant.amazon.com>
2020-09-13 16:03:00 +0100

    chaned cur granualrity to hourly but as a var

--- aws_terraform/source/cur/cur.py
+++ aws_terraform/source/cur/cur.py
@@ -9,6 +9,7 @@ def lambda_handler(event, context):
     
     S3BucketName = os.environ['BUCKET_NAME']
     Region = os.environ['REGION']
+    TimeUnit = os.environ['TIMEUNIT']
     print  (S3BucketName)
    
     response = client.describe_report_definitions()
@@ -17,7 +18,7 @@ def lambda_handler(event, context):
     response = client.put_report_definition(
       ReportDefinition={
           'ReportName': 'mybillingreport',
-          'TimeUnit': 'DAILY',
+          'TimeUnit': TimeUnit,
           'Format': 'Parquet',
           'Compression': 'Parquet',
           'AdditionalSchemaElements': [
